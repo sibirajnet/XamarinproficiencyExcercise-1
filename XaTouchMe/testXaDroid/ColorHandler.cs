7d7eb1117d03c5052d2be3777088761850520ea2
sibirajnet <sibiraj.net@gmail.com>
2016-05-27 20:01:00 +0400

    Removed unnecessary codes and comments
    Fine tuned colorHandler and uploading new apk files

--- XaTouchMe/testXaDroid/ColorHandler.cs
+++ XaTouchMe/testXaDroid/ColorHandler.cs
@@ -7,11 +7,6 @@ namespace testXaDroid
 {
 	public class ColorHandler
 	{
-		public ColorHandler ()
-		{
-
-		}
-
 		public static  ColorPattern FetchColor ()
 		{
 			// Create an HTTP web request using the URL:
@@ -21,23 +16,20 @@ namespace testXaDroid
 			request.ContentType = "application/json";
 			request.Method = "GET";
 			try {
-
-		
-				// Send the request to the server and wait for the response:
+				// Send the request to the server and get the response:
 				using (WebResponse response = request.GetResponse ()) {
 					// Get a stream representation of the HTTP web response:
 					using (Stream stream = response.GetResponseStream ()) {
-						// Use this stream to build a JSON document object:
-						//JsonValue jsonDoc = await Task.Run (() => JsonObject.Load (stream));
-						//Console.Out.WriteLine("Response: {0}", jsonDoc.ToString ());
-
-						// Return the JSON document:
-						StreamReader reader = new StreamReader (stream);
 						// Read the content.
+						StreamReader reader = new StreamReader (stream);
 						string responseFromServer = reader.ReadToEnd ();
+
+						//Load XML
 						XmlDocument doc = new XmlDocument ();
 						doc.LoadXml (responseFromServer);
 						XmlNodeList colorsList = doc.SelectNodes ("colors");
+
+						//Get Color Elements
 						foreach (XmlNode colors in colorsList) {
 							XmlNodeList ColorNodeList = colors.SelectNodes ("color");
 							foreach (XmlNode color in ColorNodeList) {
@@ -47,83 +39,30 @@ namespace testXaDroid
 									ReturnColorPattern.G = Convert.ToInt16 (RGB ["green"].InnerText);
 									ReturnColorPattern.B = Convert.ToInt16 (RGB ["blue"].InnerText);
 								}
-
 							}
-
-
 						}
-
 					}
 				}
-				if (ReturnColorPattern.R == 0 && ReturnColorPattern.G == 0 && ReturnColorPattern.G==0 ) {
+				if (ReturnColorPattern.R == 0 && ReturnColorPattern.G == 0 && ReturnColorPattern.G == 0) {
+					//No internet connection or invalid xml
+					//Fill Random colors
 					Random randonGen = new Random ();
-					ReturnColorPattern.R=randonGen.Next (256);
-					ReturnColorPattern.G=randonGen.Next (256);
-					ReturnColorPattern.B=randonGen.Next (256);
+					ReturnColorPattern.R = randonGen.Next (256);
+					ReturnColorPattern.G = randonGen.Next (256);
+					ReturnColorPattern.B = randonGen.Next (256);
 				}
 			} catch {
-				//Something went worng
-				if (ReturnColorPattern.R == 0 && ReturnColorPattern.G == 0 && ReturnColorPattern.G==0 ) {
+				    //Something went worng
+				    //Fill Random colors
 					Random randonGen = new Random ();
-					ReturnColorPattern.R=randonGen.Next (256);
-					ReturnColorPattern.G=randonGen.Next (256);
-					ReturnColorPattern.B=randonGen.Next (256);
-				}
+					ReturnColorPattern.R = randonGen.Next (256);
+					ReturnColorPattern.G = randonGen.Next (256);
+					ReturnColorPattern.B = randonGen.Next (256);
+
 			}
-
 			return ReturnColorPattern;
-
 		}
-		//		public static  ColorPattern FetchColor (string url)
-		//		{
-		//			// Create an HTTP web request using the URL:
-		//			ColorPattern ReturnColorPattern = new ColorPattern ();
-		//			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (new Uri (url));
-		//			request.ContentType = "application/json";
-		//			request.Method = "GET";
-		//
-		//			// Send the request to the server and wait for the response:
-		//			using (WebResponse response =  request.GetResponse ())
-		//			{
-		//				// Get a stream representation of the HTTP web response:
-		//				using (Stream stream = response.GetResponseStream ())
-		//				{
-		//					// Use this stream to build a JSON document object:
-		//					//JsonValue jsonDoc = await Task.Run (() => JsonObject.Load (stream));
-		//					//Console.Out.WriteLine("Response: {0}", jsonDoc.ToString ());
-		//
-		//					// Return the JSON document:
-		//					StreamReader reader = new StreamReader (stream);
-		//					// Read the content.
-		//					string responseFromServer = reader.ReadToEnd ();
-		//					XmlDocument doc = new XmlDocument ();
-		//					doc.LoadXml (responseFromServer);
-		//					XmlNodeList colorsList = doc.SelectNodes("colors");
-		//					foreach (XmlNode colors in colorsList)
-		//					{
-		//						XmlNodeList ColorNodeList = colors.SelectNodes("color");
-		//						foreach (XmlNode color in ColorNodeList)
-		//						{
-		//							XmlNodeList RGBNodeList = color.SelectNodes("rgb");
-		//							foreach (XmlNode RGB in RGBNodeList)
-		//							{
-		//								ReturnColorPattern.R =Convert.ToInt16(RGB["red"].InnerText);
-		//								ReturnColorPattern.G = Convert.ToInt16(RGB["green"].InnerText);
-		//								ReturnColorPattern.B = Convert.ToInt16(RGB["blue"].InnerText);
-		//							}
-		//
-		//						}
-		//
-		//
-		//					}
-		//
-		//				}
-		//			}
-
-		//			return ReturnColorPattern;
-		//		}
 	}
-
 	public class ColorPattern
 	{
 		public int R { get; set; }
